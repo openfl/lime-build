36e011fd23078e126ecaa5f04b49deb2bd46475e
Joshua Granick <joshua@joshuagranick.com>
2013-12-06 15:12:56 -0800

    Merge SDL2.0.1 build script changes

--- src/patches/SDL2/SDL_stdinc.h
+++ src/patches/SDL2/SDL_stdinc.h
@@ -97,7 +97,7 @@
  *  Use proper C++ casts when compiled as C++ to be compatible with the option
  *  -Wold-style-cast of GCC (and -Werror=old-style-cast in GCC 4.2 and above).
  */
-/*@{*/
+/* @{ */
 #ifdef __cplusplus
 #define SDL_reinterpret_cast(type, expression) reinterpret_cast<type>(expression)
 #define SDL_static_cast(type, expression) static_cast<type>(expression)
@@ -107,7 +107,7 @@
 #define SDL_static_cast(type, expression) ((type)(expression))
 #define SDL_const_cast(type, expression) ((type)(expression))
 #endif
-/*@}*//*Cast operators*/
+/* @} *//* Cast operators */
 
 /* Define a four character code as a Uint32 */
 #define SDL_FOURCC(A, B, C, D) \
@@ -119,7 +119,7 @@
 /**
  *  \name Basic data types
  */
-/*@{*/
+/* @{ */
 
 typedef enum
 {
@@ -161,7 +161,7 @@ typedef int64_t Sint64;
  */
 typedef uint64_t Uint64;
 
-/*@}*//*Basic data types*/
+/* @} *//* Basic data types */
 
 
 #define SDL_COMPILE_TIME_ASSERT(name, x)               \
